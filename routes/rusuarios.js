b0fae112caa380320ab5851ed582c938eb5d38fc
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-04-15 21:54:41 +0200

    W1 registro usuario completo

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -4,28 +4,66 @@ module.exports = function (app, swig, gestorBD) {
     });
 
     app.get("/signin", function (req, res) {
-        let respuesta = swig.renderFile('views/register.html', {});
+        let respuesta = swig.renderFile('views/register.html', {
+            mensaje:req.session.mensaje,
+            tipoMensaje:req.session.tipoMensaje
+        });
+        req.session.mensaje = null;
+        req.session.tipoMensaje = null;
         res.send(respuesta);
     });
 
-    app.post('/usuario', function (req, res) {
-        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        let usuario = {
-            email: req.body.email,
-            password: seguro
+    app.post('/signin', function (req, res) {
+
+        req.session.tipoMensaje = "alert-danger";
+
+        if(!req.body.email){
+            req.session.mensaje = "Error al registrar usuario: email vacio";
+            res.redirect("/signin");
         }
-        gestorBD.insertarUsuario(usuario, function (id) {
-            if (id == null) {
-                req.session.mensaje = "Error al registrar usuario";
-                req.session.tipoMensaje = "alert-danger";
-                res.redirect("/error");
-            } else {
-                req.session.mensaje = "Nuevo usuario registrado";
-                req.session.tipoMensaje = "alert-success";
-                res.redirect('/login');
-            }
-        });
+        else if(!req.body.name){
+            req.session.mensaje = "Error al registrar usuario: nombre vacio";
+            res.redirect("/signin");
+        }
+        else if(!req.body.surname){
+            req.session.mensaje = "Error al registrar usuario: apellidos vacios";
+            res.redirect("/signin");
+        }
+        else if(!(String(req.body.password) === String(req.body.passwordConfirmation))){
+            req.session.mensaje = "Error al registrar usuario: las contraseñas no coinciden";
+            res.redirect("/signin");
+        }
+        else {
+
+            let criterio = {email: req.body.email};
+            gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+                if (usuarios == null || usuarios.length === 0) {
+
+                    let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+                        .update(req.body.password).digest('hex');
+                    let usuario = {
+                        email: req.body.email,
+                        password: seguro,
+                        name: req.body.name,
+                        surname: req.body.surname
+                    };
+                    gestorBD.insertarUsuario(usuario, function (id) {
+                        if (id == null) {
+                            req.session.mensaje = "Error al registrar usuario: Hay un problema con nuestros servidores :(";
+                            req.session.tipoMensaje = "alert-danger";
+                            res.redirect("/error");
+                        } else {
+                            req.session.mensaje = "Nuevo usuario registrado";
+                            req.session.tipoMensaje = "alert-success";
+                            res.redirect('/login');
+                        }
+                    });
+                } else {
+                    req.session.mensaje = "Error al registrar usuario: No puede registrarse con ese email";
+                    res.redirect("/signin");
+                }
+            });
+        }
     });
 
     app.get("/login", function (req, res) {
