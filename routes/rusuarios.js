51d5560011de9cf07e94c44e5082ba3f764eeb23
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-04-15 22:03:14 +0200

    W2 iniciar sesion completo

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -5,8 +5,8 @@ module.exports = function (app, swig, gestorBD) {
 
     app.get("/signin", function (req, res) {
         let respuesta = swig.renderFile('views/register.html', {
-            mensaje:req.session.mensaje,
-            tipoMensaje:req.session.tipoMensaje
+            mensaje: req.session.mensaje,
+            tipoMensaje: req.session.tipoMensaje
         });
         req.session.mensaje = null;
         req.session.tipoMensaje = null;
@@ -17,23 +17,19 @@ module.exports = function (app, swig, gestorBD) {
 
         req.session.tipoMensaje = "alert-danger";
 
-        if(!req.body.email){
+        if (!req.body.email) {
             req.session.mensaje = "Error al registrar usuario: email vacio";
             res.redirect("/signin");
-        }
-        else if(!req.body.name){
+        } else if (!req.body.name) {
             req.session.mensaje = "Error al registrar usuario: nombre vacio";
             res.redirect("/signin");
-        }
-        else if(!req.body.surname){
+        } else if (!req.body.surname) {
             req.session.mensaje = "Error al registrar usuario: apellidos vacios";
             res.redirect("/signin");
-        }
-        else if(!(String(req.body.password) === String(req.body.passwordConfirmation))){
+        } else if (!(String(req.body.password) === String(req.body.passwordConfirmation))) {
             req.session.mensaje = "Error al registrar usuario: las contraseñas no coinciden";
             res.redirect("/signin");
-        }
-        else {
+        } else {
 
             let criterio = {email: req.body.email};
             gestorBD.obtenerUsuarios(criterio, function (usuarios) {
@@ -67,38 +63,53 @@ module.exports = function (app, swig, gestorBD) {
     });
 
     app.get("/login", function (req, res) {
-        let respuesta = swig.renderFile('views/login.html',  {
-            mensaje:req.session.mensaje,
-            tipoMensaje:req.session.tipoMensaje
+        let respuesta = swig.renderFile('views/login.html', {
+            mensaje: req.session.mensaje,
+            tipoMensaje: req.session.tipoMensaje
         });
+        req.session.mensaje = null;
+        req.session.tipoMensaje = null;
         res.send(respuesta);
     });
 
-    app.post("/identificarse", function (req, res) {
-        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        let criterio = {
-            email: req.body.email,
-            password: seguro
-        }
-        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
-            if (usuarios == null || usuarios.length == 0) {
-                req.session.usuario = null;
-                req.session.mensaje = "Email o password incorrecto";
-                req.session.tipoMensaje = "alert-danger";
-                res.redirect("/error");
-            } else {
-                req.session.usuario = usuarios[0].email;
-                req.session.favoritos = [];
-                res.redirect("/");
+    app.post("/login", function (req, res) {
+        if (!req.body.email) {
+            req.session.tipoMensaje = "alert-danger";
+            req.session.mensaje = "Error al identificar usuario: email vacio";
+            res.redirect("/login");
+        } else if (!req.body.password) {
+            req.session.tipoMensaje = "alert-danger";
+            req.session.mensaje = "Error al identificar usuario: contraseña vacia";
+            res.redirect("/login");
+        } else {
+            let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            let criterio = {
+                email: req.body.email,
+                password: seguro
             }
-        });
+            gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+                if (usuarios == null || usuarios.length === 0) {
+                    req.session.usuario = null;
+                    req.session.mensaje = "Email o password incorrecto";
+                    req.session.tipoMensaje = "alert-danger";
+                    res.redirect("/login");
+                } else {
+                    req.session.usuario = usuarios[0].email;
+                    req.session.favoritos = [];
+                    res.redirect("/");
+                }
+            });
+        }
     });
 
     app.get('/logout', function (req, res) {
         req.session.usuario = null;
-        res.send("Usuario desconectado");
-    })
+        req.session.mensaje = "Usuario desconectado";
+        req.session.tipoMensaje = "alert-success";
+        res.redirect('/login');
+   });
+
 
 
 };
