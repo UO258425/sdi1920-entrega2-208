53842ee095d059a34bd0637d245e8fd0303627ac
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-04-16 03:11:28 +0200

    S2 listar amigos completo

--- routes/api/rapiamigos.js
+++ routes/api/rapiamigos.js
@@ -1,5 +1,19 @@
 module.exports = function (app, gestorBD) {
 
 
+    app.get("/api/amigos", function (req, res) {
+        gestorBD.obtenerUsuarios({}, function (usuario) {
+
+            gestorBD.obtenerUsuarios({email: res.usuario}, function (usuario) {
+                gestorBD.obtenerUsuarios({email: {$in: usuario[0].friends}}, function (friends) {
+                    let friendsIDs = friends.map(friend => friend._id);
+                    res.status(200);
+                    res.json(friendsIDs);
+                });
+            });
+
+        });
+    });
+
 
 };
